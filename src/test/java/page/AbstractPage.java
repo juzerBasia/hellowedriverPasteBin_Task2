8ba6772f653529468f51ee33f65294333d0a91eb
Basia <b4605634@gmail.com>
2023-10-06 12:03:23 +0200

    .gitignore updated

--- src/test/java/page/AbstractPage.java
+++ src/test/java/page/AbstractPage.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.support.PageFactory;
 
 public abstract class AbstractPage {
 
-    protected abstract AbstractPage openPage() throws InterruptedException;
     protected final int WAIT_TIME_SECONDS = 10;
+    protected abstract AbstractPage openPage() throws InterruptedException;
 
     protected AbstractPage() {
         PageFactory.initElements(DriverManager.getDriver(), this);
